fb1be9ccd9d55b882f814bda17a108f345881f3c
Admin <smbarge@gmail.com>
2024-03-20 17:34:41 +0530

    publish btn change

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -400,16 +400,14 @@
 <div class="flex justify-end p-2 mx-10 rounded-lg">
   <div class="flex gap-2">
     <!-- <Toggle size="custom" {customSize}>Publish</Toggle> -->
-    <!-- <Button on:click={() => (popupModal = true)} color="primary">Publish</Button
-    > -->
-    <button on:click={publish} class="bg-primary-700 p-2 px-8 rounded-lg text-white"
+     <Button on:click={() => (popupModal = true)} color="primary">Publish</Button
+    >
+    <!-- <button on:click={publish} class="bg-primary-700 p-2 px-8 rounded-lg text-white"
     >Publish
-   
-    
     </button
-  >
+  > -->
     <div on:click={clearDb} class=" flex  bg-primary-700 p-2 px-8 rounded-lg text-white"
-      > CLEAR (db)
+      > CLEAR
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
       </svg>
@@ -419,7 +417,7 @@
   </div>
 </div>
 
-<!-- <Modal bind:open={popupModal} size="xs" autoclose>
+<Modal bind:open={popupModal} size="xs" autoclose>
   <div class="text-center">
     <ExclamationCircleOutline
       class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
@@ -427,10 +425,12 @@
     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
       Are you sure you want to Publish this ?
     </h3>
-    <Button color="primary" class="me-2">Yes, I'm sure</Button>
+    <Button
+    on:click={publish}
+    color="primary" class="me-2">Yes, I'm sure</Button>
     <Button color="alternative">No, cancel</Button>
   </div>
-</Modal> -->
+</Modal>
 
 {#if dataLoaded}
   <div class="bg-gray-200 p-2 rounded-md m-8">
